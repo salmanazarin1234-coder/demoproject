9686fc0a8f8eb96535596423b598cf3f48983695
Shajin <Shajin@HP>
2025-10-27 11:52:46 +0530

    24/10/25

---
+++ Samplemavenproject/src/main/java/polymorphism/Compiletimepolymorphismeg.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class Compiletimepolymorphismeg {
+	public void display(int a)
+	{
+		System.out.println(a);
+	
+	}
+public void display(String b)
+{
+	System.out.println(b);
+}
+}
---
+++ Samplemavenproject/src/main/java/polymorphism/compiletimeeg2.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class compiletimeeg2 {
+
+	public static void main(String[] args) {
+		Compiletimepolymorphismeg obj=new Compiletimepolymorphismeg();
+		obj.display(2);
+		obj.display("fyra");
+		// TODO Auto-generated method stub
+
+	}
+
+}
