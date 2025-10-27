04a3e2ddf1f5ea2ccace813ad83ff04aa531a267
Shajin <Shajin@HP>
2025-10-27 12:17:02 +0530

    27/10/25

--- Samplemavenproject/src/main/java/singleinheritance/Inheritanceexample.java
+++ Samplemavenproject/src/main/java/singleinheritance/Inheritanceexample.java
@@ -3,6 +3,15 @@ package singleinheritance;
 public class Inheritanceexample {
 	int a=5;
 	int b=2;
+	public Inheritanceexample()
+	{
+		System.out.println("Parent class constructor");
+	}
+	public Inheritanceexample(String S)
+	{
+		this();
+		System.out.println("This is paramaterized constructor:"+S);
+	}
 	public void sum()
 	{
 		int c=a+b;
--- Samplemavenproject/src/main/java/singleinheritance/sub.java
+++ Samplemavenproject/src/main/java/singleinheritance/sub.java
@@ -2,16 +2,24 @@ package singleinheritance;
 
 public class sub extends Inheritanceexample
 {
-	public void sub()
+	int a=20;
+	public sub()
 	{
+		super("hello");
+		System.out.println("child class constructor");
+	}
+	public void sub() {
+	//super.sum();
 		int d=b-a;
 		System.out.println(d);
+		System.out.println("the value of a:"+super.a);
+		
 	}
 
 	public static void main(String[] args) {
 		sub obj=new sub();
-				obj.sum();
-				obj.sub();
+				//obj.sum();
+				//obj.sub();
 				
 		// TODO Auto-generated method stub
 
